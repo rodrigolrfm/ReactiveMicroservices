c089c743eeaeb00a84b43fbb3a6274575b280a18
rodrigolrfm <31398550+rodrigolrfm@users.noreply.github.com>
2024-01-09 01:28:42 -0500

    Add Eureka Server Microservice

---
+++ EurekaServerService/src/main/java/com/middevs/reactor/eureka/app/EurekaServerServiceApplication.java
@@ -0,0 +1,14 @@
+package com.middevs.reactor.eureka.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerServiceApplication.class, args);
+    }
+
+}
