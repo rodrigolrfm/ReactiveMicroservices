df7c97a6ba5de82cd1170507d3bc305422cdb35d
rodrigolrfm <31398550+rodrigolrfm@users.noreply.github.com>
2024-01-09 13:15:07 -0500

    Added unit test

--- ClientService/src/main/java/com/middevs/reactor/client/app/ClientServiceApplication.java
+++ ClientService/src/main/java/com/middevs/reactor/client/app/ClientServiceApplication.java
@@ -2,7 +2,9 @@ package com.middevs.reactor.client.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ClientServiceApplication {
 
--- ProductoService/src/test/java/com/middevs/reactor/spring/app/ProductoServiceApplicationTests.java
+++ ProductoService/src/test/java/com/middevs/reactor/spring/app/ProductoServiceApplicationTests.java
@@ -1,13 +1,34 @@
 package com.middevs.reactor.spring.app;
 
+import com.middevs.reactor.spring.app.document.Producto;
+import com.middevs.reactor.spring.app.service.ProductoServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductoServiceApplicationTests {
 
+	@Autowired
+	private WebTestClient webTestClient;
+
+	
 	@Test
-	void contextLoads() {
+	public void listarTest(){
+
+		webTestClient.get().uri("/api/v2/productos")
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isOk()
+				.expectBodyList(Producto.class);
+
 	}
 
 }
